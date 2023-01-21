1326968225478cb8ff98f4bf3c10c7fda532c039
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-21 17:21:49 +0100

    Add project files.

---
+++ NikprogBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NikprogBackend.Models;
+
+namespace NikprogBackend.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<NikprogUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ NikprogBackend/Models/NikprogUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NikprogBackend.Models
+{
+    public class NikprogUser : IdentityUser
+    {
+
+    }
+}
