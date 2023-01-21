6171b8d2eb57c6ad1088f8e50162c977afa20464
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-21 21:36:02 +0100

    local user and token models created

---
+++ NikprogBackend/Models/UserHandling/LocalUserRegister.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NikprogBackend.Models.UserHandling
+{
+    public class LocalUserRegister
+    {
+        public DateTime? BirthDate { get; set; }
+
+        [StringLength(100)]
+        public string? Email { get; set; }
+
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        [StringLength(100)]
+        public string? Location { get; set; }
+
+        [StringLength(100)]
+        public string? Password { get; set; }
+
+        public Sex? Sex { get; set; }
+
+    }
+}
---
+++ NikprogBackend/Models/UserHandling/NikprogToken.cs
@@ -0,0 +1,8 @@
+﻿namespace NikprogBackend.Models.UserHandling
+{
+    public class NikprogToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
