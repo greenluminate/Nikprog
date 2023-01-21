6f5bed9106d8fe959526228a7274ca31191172fb
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-21 21:36:28 +0100

    microsoft user and token model created

---
+++ NikprogBackend/Models/SocialTokens/MSModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NikprogBackend.Models.SocialTokens
+{
+    public class MSModel
+    {
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ NikprogBackend/Models/SocialTokens/SocialToken.cs
@@ -0,0 +1,7 @@
+﻿namespace NikprogBackend.Models.SocialTokens
+{
+    public class SocialToken
+    {
+        public string Token { get; set; }
+    }
+}
