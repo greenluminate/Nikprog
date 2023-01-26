f24fac3c7294b7eb7949971f9702825552f16189
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-26 11:25:34 +0100

    login, logout with userservice added

--- NikprogBackend/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ NikprogBackend/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+
   isExpanded = false;
+
+  constructor(public userService: UserService) { }
 
   collapse() {
     this.isExpanded = false;
