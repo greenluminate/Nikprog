4d30c448e251c16c0417be419bf6ed15619c70d8
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-26 11:00:44 +0100

    configuration json files can understand environment variables by %%

---
+++ NikprogBackend/Models/Configuration/ExpandJsonConfigurationProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration.Json;
+
+namespace NikprogBackend.Models.Configuration
+{
+    public class ExpandJsonConfigurationProvider : JsonConfigurationProvider
+    {
+        public ExpandJsonConfigurationProvider(ExpandJsonConfigurationSource source)
+            : base(source) { }
+
+        public override void Load()
+        {
+            base.Load();
+            Data = Data.ToDictionary(
+                x => x.Key,
+                x => Environment.ExpandEnvironmentVariables(x.Value),
+                StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ NikprogBackend/Models/Configuration/ExpandJsonConfigurationSource.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration.Json;
+
+namespace NikprogBackend.Models.Configuration
+{
+    public class ExpandJsonConfigurationSource : JsonConfigurationSource
+    {
+        public override IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            EnsureDefaults(builder);
+            return new ExpandJsonConfigurationProvider(this);
+        }
+    }
+}
