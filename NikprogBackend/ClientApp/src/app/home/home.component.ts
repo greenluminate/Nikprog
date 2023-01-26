4595ff5e1e24100ddf42ccc86457f5598300b136
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-26 11:22:23 +0100

    force login call added

--- NikprogBackend/ClientApp/src/app/home/home.component.ts
+++ NikprogBackend/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigatorService } from '../services/navigator.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private userService: UserService, private navigate: NavigatorService) { }
+
+  ngOnInit(): void {
+    if (!this.userService.isLoggedIn()) {
+      this.navigate.forceLogin();
+    }
+  }
+
 }
