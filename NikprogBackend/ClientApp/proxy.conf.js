b006cec68579b1ad713703afa6a1721adf5306d9
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-26 11:02:56 +0100

    weatherforecast from template removed

--- NikprogBackend/ClientApp/proxy.conf.js
+++ NikprogBackend/ClientApp/proxy.conf.js
@@ -1,19 +1,18 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+const target = env.ASPNETCORE_HTTPS_PORT ? `${env.ASPNETCORE_HTTPS_PORT}` : // https://localhost:7224
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:32265';
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
       "/_configuration",
       "/.well-known",
       "/Identity",
       "/connect",
       "/ApplyDatabaseMigrations",
       "/_framework"
-   ],
+    ],
     target: target,
     secure: false,
     headers: {
