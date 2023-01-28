fa83560421bbad1566954aec64bc4661e23e2bfd
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-28 21:19:16 +0100

    http-get request replaced with cotrrseponding api service call

--- NikprogBackend/ClientApp/src/app/courses/courses.component.ts
+++ NikprogBackend/ClientApp/src/app/courses/courses.component.ts
@@ -1,32 +1,18 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, InjectionToken } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Course } from '../models/course';
+import { NikprogApiService } from '../services/nikprog-api.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent {
-
   public courses: Course[] = [];
-  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Course[]>(baseUrl + 'course', {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      })
-    }).subscribe(result => {
-      this.courses = result;
-    }, error => console.error(error));
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string, private apiService: NikprogApiService<Course>) {
+    this.apiService.getList("course").subscribe(courses => { this.courses = courses as Course[] });
   }
-
-}
-
-interface Course {
-  id: string;
-  name: string;
-  description: string;
-  difficulty: string;
-  photoUrl: string;
-  isHidden: boolean;
 }
