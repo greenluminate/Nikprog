27cb4385e67f6668cd0663729a9ee36514b43c5e
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-28 12:04:27 +0100

    base64 transformer services implemented

---
+++ NikprogBackend/Services/TransformerServices/Base64ToFileTransformer.cs
@@ -0,0 +1,12 @@
+﻿namespace NikprogServerClient.Services.TransformerServices
+{
+    public class Base64ToFileTransformer
+    {
+        public byte[] ConvertBase64StringToByteArray(string base64String, string path) // With it we lost 30% of the image size.
+        {
+            string filePath = path;
+            return Convert.FromBase64String(base64String);
+            //ToDo: method to write it: File.WriteAllBytes(filePath, Convert.FromBase64String(base64String));
+        }
+    }
+}
---
+++ NikprogBackend/Services/TransformerServices/FileToBase64StringTransformer.cs
@@ -0,0 +1,11 @@
+﻿namespace NikprogServerClient.Services.TransformerServices
+{
+    public class FileToBase64StringTransformer
+    {
+        public string ConvertFileToBase64String(string base64String, string path)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
