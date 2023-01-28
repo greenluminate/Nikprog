d2e33452f8d364df649bba0c22fe82256cac1333
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-28 12:09:03 +0100

    crud repository layer implemented

---
+++ NikprogBackend/Data/CRUDRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NikprogServerClient.Data
+{
+    public class CRUDRepository<TEntity> : ICRUDRepository<TEntity> where TEntity : class
+    {
+        internal NikprogDbContext db;
+        internal DbSet<TEntity> dbSet;
+
+        public CRUDRepository(NikprogDbContext db)
+        {
+            this.db = db;
+            dbSet = db.Set<TEntity>();
+        }
+
+        public void Create(TEntity obj)
+        {
+            dbSet.Add(obj);
+            db.SaveChanges();
+        }
+
+        public TEntity Read(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IQueryable<TEntity> ReadAll()
+        {
+            return dbSet.AsQueryable();
+        }
+
+        public void Update(TEntity obj)
+        {
+            db.Entry(obj).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            dbSet.Remove(Read(id));
+            db.SaveChanges();
+        }
+    }
+}
---
+++ NikprogBackend/Data/ICRUDRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace NikprogServerClient.Data
+{
+    public interface ICRUDRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity obj);
+        void Delete(string id);
+        TEntity Read(string id);
+        IQueryable<TEntity> ReadAll();
+        void Update(TEntity obj);
+    }
+}
