c6c69164336ffc82c9b0f3b18966dd62041a520c
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-28 12:11:28 +0100

    course material type for message implemented

---
+++ NikprogBackend/Models/CourseMaterials/MessageInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NikprogServerClient.Models.CourseMaterials
+{
+    public class MessageInfo : MaterialInfo
+    {
+        [MaxLength(3000)]
+        [Column("message")]
+        public string Message { get; set; }
+
+        public MessageInfo()
+        {
+            Message = "";
+        }
+    }
+}
