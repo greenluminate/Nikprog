e3aa7bcc261feec3f7ac1ea58dd79c93ce5e2224
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-31 15:05:06 +0100

    moduleApiService injected and getModulesByCourseId now used

--- NikprogBackend/ClientApp/src/app/course-details/course-details.component.ts
+++ NikprogBackend/ClientApp/src/app/course-details/course-details.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationStart, Router, RouterModule } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Course } from '../models/course';
 import { Module } from '../models/module';
+import { ModuleApiService } from '../services/module-api.service';
 import { NikprogApiService } from '../services/nikprog-api.service';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
-  styleUrls: ['./course-details.component.scss']
+  styleUrls: ['./course-details.component.scss'],
 })
 export class CourseDetailsComponent implements OnInit {
   public id: string = "";
@@ -16,7 +16,9 @@ export class CourseDetailsComponent implements OnInit {
   public course!: Course | null;
   public modules: Module[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute, private courseApiService: NikprogApiService<Course>, private moduleApiService: NikprogApiService<Module>) {
+  constructor(private router: Router, private route: ActivatedRoute,
+    private courseApiService: NikprogApiService<Course>,
+    private moduleApiService: ModuleApiService) {
     this.id = (this.router.getCurrentNavigation()?.extras.state)?.id;
 
     this.route.params.subscribe(params => {
@@ -25,12 +27,13 @@ export class CourseDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.courseApiService.get("course", this.id).subscribe(course => {
+    this.courseApiService.get('course', this.id).subscribe(course => {
       this.course = course;
     });
 
-    this.moduleApiService.getList("module").subscribe(modules => {
-      this.modules = modules as Module[]//ToDo: serverside: write to get modules by course id!
+    this.moduleApiService.getModulesByCourseId('module/GetModulesByCourseId', this.id).subscribe(modules => {
+      this.modules = modules as Module[]
     });
   }
+
 }
