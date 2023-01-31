a7a975409ea64ad8dc1f41c20d248826534d8d01
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-31 14:26:04 +0100

    moduleRepo implemented to get modules by courseId

---
+++ NikprogBackend/Data/IModuleRepository.cs
@@ -0,0 +1,9 @@
+﻿using NikprogServerClient.Models.CourseMaterials;
+
+namespace NikprogServerClient.Data
+{
+    public interface IModuleRepository
+    {
+        IQueryable<Module> ReadAllModulesByCourseId(string courseId);
+    }
+}
---
+++ NikprogBackend/Data/ModuleRepository.cs
@@ -0,0 +1,23 @@
+﻿using IdentityModel;
+using Microsoft.EntityFrameworkCore;
+using NikprogServerClient.Models.CourseMaterials;
+
+namespace NikprogServerClient.Data
+{
+    public class ModuleRepository : IModuleRepository
+    {
+        internal NikprogDbContext db;
+        internal DbSet<Module> moduleDbSet;
+
+        public ModuleRepository(NikprogDbContext db)
+        {
+            this.db = db;
+            this.moduleDbSet = db.Set<Module>();
+        }
+
+        public IQueryable<Module> ReadAllModulesByCourseId(string courseId)
+        {
+            return moduleDbSet.Where(m => m.CourseId == courseId);
+        }
+    }
+}
