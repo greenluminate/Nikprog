d8a7290146a65044826ce69ff5b496c99b42566f
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2023-01-31 14:49:32 +0100

    coding style offenses fixed

--- NikprogBackend/ClientApp/src/app/courses/courses.component.ts
+++ NikprogBackend/ClientApp/src/app/courses/courses.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Inject, InjectionToken } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
 import { Course } from '../models/course';
 import { NikprogApiService } from '../services/nikprog-api.service';
 
@@ -12,7 +10,7 @@ import { NikprogApiService } from '../services/nikprog-api.service';
 
 export class CoursesComponent {
   public courses: Course[] = [];
-  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string, private apiService: NikprogApiService<Course>) {
-    this.apiService.getList("course").subscribe(courses => { this.courses = courses as Course[] });
+  constructor(private apiService: NikprogApiService<Course>) {
+    this.apiService.getList('course').subscribe(courses => { this.courses = courses as Course[] });
   }
 }
